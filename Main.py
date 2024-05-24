b218e3871b42fa8bd99a7172ae1d1b9989eb172c
BlobsterGitHub <167622952+BlobsterGitHub@users.noreply.github.com>
2024-05-24 20:44:01 +0100

    Main.py

---
+++ Main.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, request
+import discord
+
+app = Flask(__name__)
+
+DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1207765094520459314/XHmffIT1sLXsGwegxlgaMkyNMb8rDWnajT4AEO_kggKqcG1o0zuRrVkSkGgqfDPN0VNS'
+
+def send_to_discord(ip_address):
+    message = 'New IP Address: ' + ip_address
+    webhook = discord.Webhook.from_url(DISCORD_WEBHOOK_URL, adapter=discord.RequestsWebhookAdapter())
+    webhook.send(message)
+
+@app.route('/')
+def index():
+    return jsonify(message="Welcome to Blobster's Test Site!")
+
+@app.route('/get_ip')
+def get_ip():
+    ip_address = request.remote_addr
+    send_to_discord(ip_address)
+    return jsonify(ip=ip_address)
+
+if __name__ == '__main__':
+    # Listen on IPv6 address :: for all interfaces
+    app.run(host='::', port=8000)
